cfd477434e69faf9ae3fb1cd600a6cba25f1def7
Dominic Tremblay <dominictremblay24@gmail.com>
2019-07-19 15:44:06 -0400

    connect to pg client setup

--- server.js
+++ server.js
@@ -1,5 +1,23 @@
 const express = require('express');
+const { Client } = require('pg');
 const port = process.env.PORT || 3000;
 const app = express();
+
+require('dotenv').config();
+
+const connectionOptions = {
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+};
+
+const pgClient = new Client(connectionOptions);
+
+pgClient
+  .connect()
+  .then(console.log(`Connected to ${pgClient.database} Database`))
+  .catch(err => console.error('connection error', err.stack));
 
 app.listen(port, () => console.log(`Server listening on port ${port}`));
